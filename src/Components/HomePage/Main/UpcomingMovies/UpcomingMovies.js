3d51907a263b42a31ff7e591d425c44ca8b4aa61
Mike <nghia00720@yahoo.com.vn>
2019-11-20 14:29:17 -0500

    Finish the first draft.

--- src/Components/HomePage/Main/UpcomingMovies/UpcomingMovies.js
+++ src/Components/HomePage/Main/UpcomingMovies/UpcomingMovies.js
@@ -5,24 +5,32 @@ export const UpcomingMovies= props =>{
     return(
         <section className='categoryMovies'>
                 <h2 className='categoryMovies__titles'>UPCOMING</h2>
-                <section className='swipeWraper'>
+                {/* <section className='swipeWraper'>
                         <button><i className='fas fa-angle-left'></i></button>
                         <button><i className='fas fa-angle-right'></i></button>
-                    </section>
+                    </section> */}
                 <section className='categoryMoviesCarousel'>
-                        
+    
                         <section className='carouselMovies'>
                                 {props.UpcomingMovies.map( item =>{
                                     return(
                                         <section className='infoMovies'>
-                                            <section className='containerImages'>
+                                            <section className='w3-display-container w3-text-white containerImages'>
                                                         <img src={item.imageMovies} />
+                                                        <section className="w3-display-topleft w3-container"><span>HD</span></section>
                                                 </section>
                                                 <p className='infoMovies__titles'>{item.nameMovies}</p>
                                                 <span className='infoMovies__categories'>{item.categoryMovies}</span>
+                                                
                                         </section>
                                     );
                                 })}
+                            </section>
+                        <span className='categoryMovies__seperator'></span>
+                        
+                        <section className='swipeWraper'>
+                                <button><i className='fas fa-angle-left'></i></button>
+                                <button><i className='fas fa-angle-right'></i></button>
                             </section>
                        
                     </section>
