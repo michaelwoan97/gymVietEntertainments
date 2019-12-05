ed87b3425526eb856d1a696b8d36929a007a7a4f
Mike <nghia00720@yahoo.com.vn>
2019-12-05 15:45:24 -0500

    Failed.

--- src/Components/HomePage/Main/UpcomingMovies/UpcomingMovies.js
+++ src/Components/HomePage/Main/UpcomingMovies/UpcomingMovies.js
@@ -7,48 +7,6 @@ export const UpcomingMovies= props =>{
     
     return(
         
-        // <section className='swiper-container' >
-        //         {/* <h2 id="swiper-container__Titles" className='wow fadeInLeft categoryMovies__titles'>UPCOMING</h2> */}
-
-        //         {/* <section className='categoryMoviesCarousel'> */}
-    
-        //                 <section className='swiper-wrapper'>
-        //                         {props.UpcomingMovies.map( (item,i) =>{
-        //                             return(
-        //                                 <section  className='swiper-slide ' key={`Movie_`+i}>
-        //                                     <section className='w3-display-container w3-text-white containerImages'>
-        //                                                 <img src={`https://image.tmdb.org/t/p/w185/${item.poster_path}`} alt={item.overview} />
-        //                                                 <section className="w3-display-topleft w3-container">
-        //                                                     <span className="material-icons" style={{ color: '#3498db'}}>language</span>
-        //                                                     <span style={{ textTransform: 'uppercase'}}>{item.original_language}</span>
-        //                                                     </section>
-        //                                         </section>
-        //                                         <p className='infoMovies__titles'>{item.title || item.name}</p>
-        //                                         <span>
-        //                                             <i className='fas fa-heart' style={{ color: '	rgba(227,27,35,0.7)'}}></i>{item.popularity}
-        //                                             <i className='fa fa-star' style={{ color: 'rgba(254,211,48,0.7)'}}></i>{item.vote_average}  
-        //                                         </span>
-                                               
-                                                
-        //                                 </section>
-        //                             );
-        //                         })}
-                                
-        //                     </section>
-                            
-        //                     <div className="swiper-button-prev"></div>
-        //                     <div className="swiper-button-next"></div>
-                        
-                        
-        //                {/* <section className='swipeWraper'>
-        //                         <button><i className='fas fa-angle-left'></i></button>
-        //                         <button><i className='fas fa-angle-right'></i></button>
-        //                     </section>  */}
-        //                 {/* <span className='categoryMovies__seperator'></span> */}
-        //             {/* </section> */}
-                    
-                  
-        // </section>
 
         <section className='categoryMovies' >
                 <h2 id="UpcommingSection__Titles" className='wow fadeInLeft categoryMovies__titles'>UPCOMING</h2>
@@ -90,4 +48,3 @@ export const UpcomingMovies= props =>{
                   
         </section>
     );
-}
